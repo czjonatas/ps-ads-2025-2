06bdb8e3e609fdbb9c27d88138b554b16552197f
Jonatas Candido Gonçalves <jonatascandido364@gmail.com>
2025-11-26 11:19:38 +0000

    atualização na tela de login

--- front-end/src/ui/FooterBar.jsx
+++ front-end/src/ui/FooterBar.jsx
@@ -26,7 +26,7 @@ export default function FooterBar() {
         }}
       >
         Desenvolvido e mantido com <LocalCafeIcon fontSize="small" /> por <a 
-        href="mailto:professor@faustocintra.com.br">Prof. Fausto G. Cintra</a>
+        href="mailto:jonatas.goncalves01@fatec.sp.gov.br.br">JONATAS CANDIDO GONCALVES</a>
       </Typography>
     </Box>
   );
